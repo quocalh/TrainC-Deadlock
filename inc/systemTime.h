a27869a3d5fb4593670772291664946bcf6bf627
quocalh <anhquocnguyen502@gmail.com>
2026-04-23 21:07:55 +0700

    time fetching API

---
+++ inc/systemTime.h
@@ -0,0 +1,27 @@
+#ifndef SYSTEM_TIME_H
+#define SYSTEM_TIME_H
+
+#include <time.h>
+
+typedef struct {
+  time_t now;
+  struct tm *time;
+  int year;
+  int month;
+  int date;
+  int hour;
+  int minute;
+  int second;
+} systemTime;
+
+void fetchTime(systemTime *system_time);
+
+int systemTime_getCurrentYear(systemTime *system_time);
+int systemTime_getCurrentMonth(systemTime *system_time);
+int systemTime_getCurrentDate(systemTime *system_time);
+
+int systemTime_getCurrentHour(systemTime *system_time);
+int systemTime_getCurrentMinute(systemTime *system_time);
+int systemTime_getCurrentSecond(systemTime *system_time);
+
+#endif
