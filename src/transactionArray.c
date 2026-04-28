54cc00f428ac31e76f1d02832244869815bc4677
quocalh <anhquocnguyen502@gmail.com>
2026-04-28 11:23:51 +0700

    [systemArray]: add the sample test case, in case it got bugged again

--- src/transactionArray.c
+++ src/transactionArray.c
@@ -70,3 +70,111 @@ void TranscationArrayFree(transactionArray *transaction_array) {
   free(transaction_array->ptr);
   printf("TransactionArray: freed\n");
 }
+// ------------------------------------------------------------------------
+// testing.c
+// // #include "inc/transaction.h"
+// #include "inc/systemTime.h"
+// #include "inc/transactionArray.h"
+// #include <stdio.h>
+//
+// int main() {
+//   transactionArray transaction_array = TransactionArrayInit(10);
+//   printf("transaction_array -> ptr: %p \n", transaction_array.ptr);
+//   printf("trasaction_array -> count: %d \n", transaction_array.count);
+//   printf("trasaction_array -> currentCapacity: %d \n",
+//          transaction_array.currentCapacity);
+//   printf("trasaction_array -> capacity: %d \n", transaction_array.capacity);
+//
+//   systemTime time_system;
+//   fetchTime(&time_system);
+//   timeStamp time_stamp;
+//   saveTick(&time_system, &time_stamp);
+//
+//   TransactionArrayAddTransaction(&transaction_array, 54, time_stamp);
+//   printf("\n");
+//   printf("transaction_array -> ptr: %p \n", transaction_array.ptr);
+//   printf("trasaction_array -> count: %d \n", transaction_array.count);
+//   printf("trasaction_array -> currentCapacity: %d \n",
+//          transaction_array.currentCapacity);
+//   printf("trasaction_array -> capacity: %d \n", transaction_array.capacity);
+//
+//   TransactionArrayAddTransaction(&transaction_array, 54, time_stamp);
+//   printf("\n");
+//   printf("transaction_array -> ptr: %p \n", transaction_array.ptr);
+//   printf("trasaction_array -> count: %d \n", transaction_array.count);
+//   printf("trasaction_array -> currentCapacity: %d \n",
+//          transaction_array.currentCapacity);
+//   printf("trasaction_array -> capacity: %d \n", transaction_array.capacity);
+//
+//   TransactionArrayAddTransaction(&transaction_array, 54, time_stamp);
+//   printf("\n");
+//   printf("transaction_array -> ptr: %p \n", transaction_array.ptr);
+//   printf("trasaction_array -> count: %d \n", transaction_array.count);
+//   printf("trasaction_array -> currentCapacity: %d \n",
+//          transaction_array.currentCapacity);
+//   printf("trasaction_array -> capacity: %d \n", transaction_array.capacity);
+//
+//   TransactionArrayAddTransaction(&transaction_array, 54, time_stamp);
+//   printf("\n");
+//   printf("transaction_array -> ptr: %p \n", transaction_array.ptr);
+//   printf("trasaction_array -> count: %d \n", transaction_array.count);
+//   printf("trasaction_array -> currentCapacity: %d \n",
+//          transaction_array.currentCapacity);
+//   printf("trasaction_array -> capacity: %d \n", transaction_array.capacity);
+//
+//   TransactionArrayAddTransaction(&transaction_array, 54, time_stamp);
+//   printf("\n");
+//   printf("transaction_array -> ptr: %p \n", transaction_array.ptr);
+//   printf("trasaction_array -> count: %d \n", transaction_array.count);
+//   printf("trasaction_array -> currentCapacity: %d \n",
+//          transaction_array.currentCapacity);
+//   printf("trasaction_array -> capacity: %d \n", transaction_array.capacity);
+//
+//   printf("\nTesting section: \n");
+//   for (unsigned int i = 0; i < transaction_array.count; i++) {
+//     Transaction *trans = transaction_array.ptr;
+//     printf("ID: %d, year: %d,second: %d\n", trans->productID,
+//            trans->time_stamp.year, trans->time_stamp.second);
+//   }
+//   TranscationArrayFree(&transaction_array);
+// }
+//
+// output:
+// ---------------------------------------------------------------------
+// transaction_array -> ptr: 0000023E93D97BA0
+// trasaction_array -> count: 0
+// trasaction_array -> currentCapacity: 1
+// trasaction_array -> capacity: 10
+//
+// transaction_array -> ptr: 0000023E93D97BA0
+// trasaction_array -> count: 1
+// trasaction_array -> currentCapacity: 1
+// trasaction_array -> capacity: 10
+//
+// transaction_array -> ptr: 0000023E93D91670
+// trasaction_array -> count: 2
+// trasaction_array -> currentCapacity: 2
+// trasaction_array -> capacity: 10
+//
+// transaction_array -> ptr: 0000023E93D9CA40
+// trasaction_array -> count: 3
+// trasaction_array -> currentCapacity: 4
+// trasaction_array -> capacity: 10
+//
+// transaction_array -> ptr: 0000023E93D9CA40
+// trasaction_array -> count: 4
+// trasaction_array -> currentCapacity: 4
+// trasaction_array -> capacity: 10
+//
+// transaction_array -> ptr: 0000023E93D9CA40
+// trasaction_array -> count: 5
+// trasaction_array -> currentCapacity: 8
+// trasaction_array -> capacity: 10
+//
+// Testing section:
+// ID: 54, year: 2026,second: 11
+// ID: 54, year: 2026,second: 11
+// ID: 54, year: 2026,second: 11
+// ID: 54, year: 2026,second: 11
+// ID: 54, year: 2026,second: 11
+// TransactionArray: freed
